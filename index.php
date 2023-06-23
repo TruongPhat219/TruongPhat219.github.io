ec444b786c7df92ad021e0ac22c9e3553a68a150
TruongPhat219 <137505898+TruongPhat219@users.noreply.github.com>
2023-06-23 18:59:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -348,10 +348,10 @@ color: #FFFFFF;
 					</p>
 					<div class="login-txt ta-c f-16 mb10">
 						<p class="ta-c mb10"><a class="contactme">CONTACT ME:</a></p>
-						<p class="ta-c mb10"><a style="font-size: 16px;" href="https://facebook.com/WusThanhDieu" target="_blank" class="tx-btn tx-btn-big dp-b bg-red">FACEBOOK</a></p>
+						<p class="ta-c mb10"><a style="font-size: 16px;" href="https://www.facebook.com/TruongPhat219" target="_blank" class="tx-btn tx-btn-big dp-b bg-red">FACEBOOK</a></p>
 						<p class="ta-c mb10"><a  style="font-size: 16px;" href="https://m.me/WusThanhDieu" target="_blank" class="tx-btn tx-btn-big dp-b bg-red">MESSENGER</a></p>
             <p class="ta-c mb10"><a  style="font-size: 16px;" href="https://zalo.me/vuongdieu2k3" target="_blank" class="tx-btn tx-btn-big dp-b bg-red">ZALO</a></p>
-						<p class="ta-c mb10"><a  style="font-size: 16px;" href="https://t.me/WusThanhDieu" target="_blank" class="tx-btn tx-btn-big dp-b bg-red">TELEGRAM</a></p>
+						<p class="ta-c mb10"><a  style="font-size: 16px;" href="Momo: 0365508140" target="_blank" class="tx-btn tx-btn-big dp-b bg-red">TELEGRAM</a></p>
 						<!-- <p class="ta-c mb10"><a href="link" target="_blank" class="tx-btn tx-btn-big zhuce">text</a></p>-->
 						<p><span style="font-size: 16px;"><b>Made by ^_^ <font color="red">name your</font></b></span></p>
 					</div>
